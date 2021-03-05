5aaf9360ab27cf00eb12c30b56c472d41c72bf34
Dominik Bucko <dominik.bucko@pan-net.eu>
2021-03-05 10:18:41 +0100

    updated user endpoint docs

--- apis/namespaces/user.py
+++ apis/namespaces/user.py
@@ -13,3 +13,65 @@ messageModel = ns.model(
         "message": fields.String,
     },
 )
+
+userModel = ns.model(
+    "User",
+    {
+        "email": fields.String,
+        "name": fields.String,
+        "nickname": fields.String,
+        "telephone": fields.String,
+    }
+)
+
+@ns.route("/users")
+class User(Resource):
+    @ns.doc(
+        description="Create new user.",
+        params={},
+        responses={
+            200: "OK",
+            400: "Bad request",
+        },
+    )
+    @ns.expect(userModel)
+    @ns.response(200, "OK", messageModel)
+    @authenticate
+    def post(self):
+        pass
+
+
+@ns.route("/users/<uid>")
+class SpecificUser(Resource):
+    @ns.doc(
+        description="Fetch user.",
+        params={},
+        responses={
+            200: "OK",
+            400: "Bad request",
+            401: "Unauthorized",
+            404: "Not found",
+
+        },
+    )
+    @ns.response(200, "OK", userModel)
+    @authenticate
+    def get(self, uid):
+        pass
+
+    @ns.doc(
+        description="Update user.",
+        params={},
+        responses={
+            200: "OK",
+            400: "Bad request",
+            401: "Unauthorized",
+            404: "Not found",
+
+        },
+    )
+    @ns.response(200, "OK", userModel)
+    @ns.expect(userModel)
+    @authenticate
+    def patch(self, uid):
+        pass
